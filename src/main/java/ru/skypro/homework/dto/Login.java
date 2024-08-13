3dab5e942c79835d88e3c3da98b87b1d5e9eacb7
Lyamzin <lyamzin01@mail.ru>
2024-08-13 18:09:44 +1000

    Добавил класс комментарии (сервис,конструктор,сущность) Изменил User,Login ,Register

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -1,10 +1,35 @@
 package ru.skypro.homework.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Objects;
+
+@Getter
+@Setter
 @Data
 public class Login {
-
+    private String id;
     private String username;
     private String password;
+
+    public Login(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Login login = (Login) o;
+        return Objects.equals(id, login.id) && Objects.equals(username, login.username) && Objects.equals(password, login.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
 }
--- src/main/java/ru/skypro/homework/dto/Register.java
+++ src/main/java/ru/skypro/homework/dto/Register.java
@@ -1,7 +1,13 @@
 package ru.skypro.homework.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Objects;
+
+@Setter
+@Getter
 @Data
 public class Register {
 
@@ -11,4 +17,26 @@ public class Register {
     private String lastName;
     private String phone;
     private Role role;
+
+    public Register(String username, String password, String firstName, String lastName, String phone, Role role) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Register register = (Register) o;
+        return Objects.equals(username, register.username) && Objects.equals(password, register.password) && Objects.equals(firstName, register.firstName) && Objects.equals(lastName, register.lastName) && Objects.equals(phone, register.phone) && role == register.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, firstName, lastName, phone, role);
+    }
 }
